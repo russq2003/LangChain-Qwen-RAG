3233f9d293cc6205e77a2936024d30a4e52288f6
russq <russellq2003@sina.com>
2024-08-13 10:45:19 +0800

    8.13

--- final.py
+++ final.py
@@ -21,17 +21,17 @@ def load_pdf(path):
 
 # 文档切片
 text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,                  # 指定每个文本块的目标大小
-    chunk_overlap=500,                # 指定文本块之间的重叠字符数
+    chunk_size=500,                  # 指定每个文本块的目标大小
+    chunk_overlap=0,                # 指定文本块之间的重叠字符数
     length_function=len,             # 用于测量文本长度的函数
     is_separator_regex=False,        # 指定`separators`中的分隔符是否应被视为正则表达式，这里设置为False，表示分隔符是字面字符
-    separators=['\n\n', '\n', ',', '.', '。','，']
+    separators=['。','，','\n\n', '\n', ',', '.',]
 )
 
 # embedding
-model_name = "sentence-transformers/all-mpnet-base-v2"   # embedding的模型，可调
+model_name = "sentence-transformers/multi-qa-mpnet-base-dot-v1"   # embedding的模型，可调
 model_kwargs = {'device': 'cpu'}
-encode_kwargs = {'normalize_embeddings': True}   # 是否对生成向量归一化
+encode_kwargs = {'normalize_embeddings': False}   # 是否对生成向量归一化
 hf = HuggingFaceEmbeddings(
     model_name=model_name,
     model_kwargs=model_kwargs,
@@ -48,9 +48,9 @@ texts = text_splitter.split_documents(pages)
 db = Chroma.from_documents(
     documents=texts,
     embedding=hf,
-    persist_directory='WINDPOWER-FINAL/chroma_db'
+    persist_directory='WINDPOWER-FINAL4/chroma_db'
 )
-db.persist()  # 立即持久化数据库
+# db.persist()  # 立即持久化数据库
 
 # Qwen模型API
 DASHSCOPE_API_KEY = 'sk-a2da8256eb84494bb91e1f27488a65cb'
@@ -58,7 +58,7 @@ os.environ["DASHSCOPE_API_KEY"] = DASHSCOPE_API_KEY
 
 # 定义系统消息
 system_message = SystemMessagePromptTemplate.from_template(
-    "你是一个电力行业专家，请回答用户问题，如果遇到与电力不相关的问题请回答该问题与电力不相关。对于不知道的问题就请回答不知道。"
+    "你是一个电力行业专家，请回答用户问题，如果遇到与电力不相关的问题请回答该问题与电力不相关。对于不知道的问题就请礼貌地回答不知道，尽最大可能回答用户问题。"
 )
 
 # 定义人类消息
@@ -69,15 +69,19 @@ human_message = HumanMessagePromptTemplate.from_template(human_template)
 chat_prompt = ChatPromptTemplate.from_messages([system_message, human_message])
 
 # llm
-llm = Tongyi(model_name='qwen-max', temperature=1)  # 基础模型可以改
+llm = Tongyi(model_name='qwen-max', temperature=1)  # 基础模型可以改，temperature改变模型回答特点
 chain = chat_prompt | llm
 
 messages = []
+MAX_messages = 5
 print("你好，我是电力行业专家，有任何关于电力的专业问题都可以向我提问！")
 while True:
     # 定义问题
     question = input("user:")
     messages.append({'role': Role.USER, 'content': question})
+# 控制消息列表长度
+    if len(messages) > MAX_messages:
+        messages = messages[-MAX_messages:]  # 保留最后MAX_MESSAGES_HISTORY条消息
 
     # 相似度方法通过查询文本检索数据
     similarDocs = db.similarity_search(question, k=20)
@@ -87,7 +91,7 @@ while True:
         # print("找到相关文档。")
         summary_prompt = "".join([doc.page_content for doc in similarDocs])
 
-        send_message = f"下面的信息({summary_prompt})是否有这个问题({question})有关，如果你觉得无关请直接做出自己的回答，不要说与提供的信息无关；否则请根据{summary_prompt}对{question}的问题进行回答"
+        send_message = f"你可以参考一下信息：({summary_prompt})回答({question})这个问题，如果没有找到与({question})相关的信息请如实回答，不要编造"
         messages.append({'role': Role.USER, 'content': send_message})
 
         # 调用 chain.invoke
